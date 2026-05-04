4bf70b62047ec8a948b811d641c46ed95377d4fc
Karl <karl@hundal.com>
2026-05-04 18:29:07 -0400

    got that thang on it, search bar works. Now to css.

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,6 +2,18 @@ export default function SearchBar() {
   return (
     <>
       <h1>hello from searchbar</h1>
+      <div id="search-bar">
+        <label for="movie-search"></label>
+        <input
+          type="search"
+          name="movie-search"
+          placeholder="Search for a movie"
+          id="movie-search"
+        />
+        <button type="submit" form="movie-search-form" id="search-button">
+          Search
+        </button>
+      </div>
     </>
   );
 }
