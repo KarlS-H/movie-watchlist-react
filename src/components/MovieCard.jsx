9aca4040e3329087943f0ba2501595a58731c1e9
Karl <karl@hundal.com>
2026-05-05 23:55:02 -0400

    reactify elements, import useeffect and usestate for fetch request and setting data.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,3 +1,53 @@
+import { useEffect, useState } from "react";
+
 export default function MovieCard({ movie }) {
-  return <></>;
+  const apiKey = import.meta.env.VITE_PUBLIC_OMDB_KEY;
+  const [idData, setIdData] = useState(null);
+  useEffect(() => {
+    fetch(`https://www.omdbapi.com/?i=${movie.imdbID}&apikey=${apiKey}`)
+      .then((res) => res.json())
+      .then((data) => setIdData(data));
+  }, [movie.imdbID]);
+
+  const storedWatchlist = parseJSON();
+  const isMovieSaved = storedWatchlist?.some(
+    (saved) => saved.imdbID === movie.imdbID,
+  );
+  const watchlistButton = isMovieSaved
+    ? `<i className="fa-solid fa-circle-minus"></i> Remove`
+    : `<i className="fa-solid fa-circle-plus"></i> Watchlist`;
+  return (
+    <>
+      <div className="movie">
+        <div className="movie-image">
+          <img className="image " src={movie.Poster} />
+        </div>
+
+        <div className="movie-bio">
+          <div className="title-rating">
+            <p className="movie-title">{movie.Title}</p>
+            <i className="fa-solid fa-star"></i>
+            <p className="movie-rating">
+              {idData.Ratings?.[0]?.Value || "N/A"}
+            </p>
+          </div>
+
+          <div className="movie-details">
+            <div className="length-genres-watchlist">
+              <div className="runtime">{idData.Runtime || "N/A"}</div>
+              <div className="genre">{idData.Genre || "N/A"}</div>
+              <button className="watchlist-btn" data-imdb-id={movie.imdbID}>
+                {watchlistButton}
+              </button>
+            </div>
+          </div>
+
+          <div className="film-description">
+            <p>{idData.Plot || "No description available"}</p>
+          </div>
+        </div>
+      </div>
+      <hr className="bottom-line" />
+    </>
+  );
 }
