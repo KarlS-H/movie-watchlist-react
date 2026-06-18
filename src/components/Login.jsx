478a59d33f54aadfbf9c23c7a5022808b0710326
Karl <karl@hundal.com>
2026-06-18 17:36:34 -0400

    login component and register ccomponent init

---
+++ src/components/Login.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./App.css";
+function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+}
+const onSubmit = (data) => {
+  const userData = JSON.parse(localstorage.getItem(data.email));
+  if (userData) {
+    if (userData.password === data.password) {
+      console.log(userData.name + "You are successfully logged in");
+    } else {
+      console.log("Email or Password is does not match");
+    }
+  }
+
+  return (
+    <>
+      <h2>Login Form</h2>
+      <form className="App" onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="email"
+          {...register("email", { required: true })}
+          placeholder="Email"
+        />
+        {errors.email && (
+          <span style={{ color: "red" }}>Email is required</span>
+        )}
+        <input
+          type="password"
+          {...register("password", { required: true })}
+          placeholder="Password"
+        />
+        {errors.password && (
+          <span style={{ color: "red" }}>*Password* is mandatory</span>
+        )}
+        <input type="submit" style={{ backgroundColor: "#a1eafb" }} />
+      </form>
+    </>
+  );
+};
+
+export default Login;
